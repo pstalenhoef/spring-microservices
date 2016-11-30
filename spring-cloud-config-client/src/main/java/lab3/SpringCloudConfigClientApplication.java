29426388f3f71fe11eda51642c2f6d67b59f7776
pstalenhoef <paul.stalenhoef@gmail.com>
2016-11-30 19:34:28 +0100

    Added a controller to show the word from the config

--- spring-cloud-config-client/src/main/java/lab3/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/lab3/SpringCloudConfigClientApplication.java
@@ -2,8 +2,10 @@ package lab3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"lab3.web.controller"})
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
