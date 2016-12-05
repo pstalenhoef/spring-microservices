25f3dcb50d90dec61d3ef3dc4a2ef7d92236636e
p.stalenhoef@everest.nl <p.stalenhoef@everest.nl>
2016-12-05 20:23:36 +0100

    Optimized imports

--- lab-5/spring-cloud-eureka-client-noun/src/main/java/lab4/web/controller/WordController.java
+++ lab-5/spring-cloud-eureka-client-noun/src/main/java/lab4/web/controller/WordController.java
@@ -1,6 +1,5 @@
 package lab4.web.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
