a1228bc942ecdda8a235e0285c1f1fe9bfe36d79
p.stalenhoef@everest.nl <p.stalenhoef@everest.nl>
2016-12-14 21:08:00 +0100

    Added lab-6 projects

---
+++ lab-6/spring-cloud-config-server/src/main/java/lab3/Lab3Application.java
@@ -0,0 +1,14 @@
+package lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3Application.class, args);
+	}
+}
